9a8998e80f15eab1c8321bd5dcd4f524bc616f8d
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-08-08 20:10:35 +0530

    Added Required dependancies

--- src/main/java/com/students/StudentsManagement/exceptionHandeler/GlobalExceptionHandler.java
+++ src/main/java/com/students/StudentsManagement/exceptionHandeler/GlobalExceptionHandler.java
@@ -1,4 +1,13 @@
 package com.students.StudentsManagement.exceptionHandeler;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
 }
