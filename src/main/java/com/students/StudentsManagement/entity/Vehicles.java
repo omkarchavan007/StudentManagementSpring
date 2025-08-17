f07c482b4902b4a184df25aee65abd7832e0f400
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-08-17 22:26:28 +0530

    Added Entities in Vehicles class

--- src/main/java/com/students/StudentsManagement/entity/Vehicles.java
+++ src/main/java/com/students/StudentsManagement/entity/Vehicles.java
@@ -1,4 +1,45 @@
 package com.students.StudentsManagement.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+
 public class Vehicles {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long vehicleId;
+
+    private String vehicleType;
+    private String registrationNumber;
+
+
+
+
+    public Long getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(Long vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
 }
